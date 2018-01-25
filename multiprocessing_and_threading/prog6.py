4c5a3bedfc06175c0c5db6670e1c87927ae5f863
Mrinal Sinha <themrinalsinha@gmail.com>
2018-01-25 16:38:40 +0530

    removed garbage code

--- multiprocessing_and_threading/prog6.py
+++ multiprocessing_and_threading/prog6.py
@@ -1,17 +1,4 @@
 # Multiprocessing pool
-
-def f(n):
-    return n * n
-
-if __name__ == '__main__':
-    array = [2,5,6,7,8,3]
-
-    result = []
-    for n in array:
-        result.append(f(n))
-
-    print(result)
-
 # Generally, the function loads the above function in a any of core and process the input and rest all the cores are sitting idle.
 
 # Now if somehow if you can deploy your code on all the cores and divide you input between all the cores and then after processing aggregate all the results together
