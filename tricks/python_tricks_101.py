4ad0a7b42002bd8e76c6065016123bd624699381
Mrinal Sinha <themrinalsinha@gmail.com>
2019-04-16 15:57:18 +0530

    Finding most frequest value in list

--- tricks/python_tricks_101.py
+++ tricks/python_tricks_101.py
@@ -10,3 +10,14 @@ a = ['Hello', 'world', 'this', 'is', 'mrinal']
 print(' '.join(a))
 
 
+# Finding most frequest value in list
+# ---- most frequest element in a list -----
+a = [1, 2, 3, 1, 2, 3, 2, 2, 4, 5, 1]
+print('Set gives all unique element from list: ', set(a))
+print('Max (without key) given max value in that list: ', max(set(a)))
+print('Finding max base on number of repetation: ', max(set(a), key=a.count))
+# ---- using counter from collection -----
+from collections import Counter
+cnt = Counter(a)
+print('counter output: ',  cnt)
+print('most frequest in counter: ', cnt.most_common(3))
