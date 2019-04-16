d4d1e9dd9e088f61da04cc5548025409f00beb4a
Mrinal Sinha <themrinalsinha@gmail.com>
2019-04-16 18:56:23 +0530

    for else

--- tricks/python_tricks_101.py
+++ tricks/python_tricks_101.py
@@ -100,3 +100,13 @@ from operator import itemgetter
 print(sorted(d.items(), key=itemgetter(1)))
 # ---- sort dict keys by value ----
 print(sorted(d, key=d.get))
+
+
+# For Else
+# ---- else gets called when for loop does not reach break statement ----
+a = [1, 2, 3, 4, 5]
+for elem in a:
+    if elem == 0:
+        break
+else:
+    print('did not break out of for loop')
