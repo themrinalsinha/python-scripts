ada5c63b7648d07a9d909d2d2085e99816fb2631
Mrinal Sinha <themrinalsinha@gmail.com>
2019-06-05 23:27:45 +0530

    cutting pasting and merging images

--- pil/learn_1.py
+++ pil/learn_1.py
@@ -31,3 +31,22 @@ size = (256, 256)
 img  = Image.open('sample.jpg')
 img.thumbnail(size)
 img.save('icon.thumbnail', "JPEG")
+
+# ====================================================
+# Cutting, pasting and merging images.
+# The Image class containing methods allowing you to manipulate regions
+# within an image. To extract a sub-rectangle from an image, use the crop() method.
+img = Image.open('sample.jpg')
+box = (300, 300, 700, 700)
+region = img.crop(box)
+# region.save('cut.jpg')
+# Image.open('cut.jpg').show()
+
+# The region could now be processed in certain manner and pasted back.
+region = region.transpose(Image.ROTATE_180)
+img.paste(region, box)
+# When pasting region back, the size of region must match the given region exactly.
+# the region cannot extend outside the image. However the mode of original image and
+# region do not need to match.
+img.save('rotate_region.jpg')
+# ====================================================
