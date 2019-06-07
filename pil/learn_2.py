48e4fece9e1f49155350fc7e3c42b06d3daf4332
Mrinal Sinha <themrinalsinha@gmail.com>
2019-06-07 15:42:39 +0530

    more on reading images

--- pil/learn_2.py
+++ pil/learn_2.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from PIL import PSDraw
+from PIL import TarIO, ContainerIO
 
 im = Image.open("sample.jpg")
 title = "hopper"
@@ -17,4 +18,22 @@ ps.setfont("HelveticaNarrow-Bold", 36)
 ps.text((3*72, 4*72), title)
 
 ps.end_document()
-im.show()
+# im.show()
+# ====================================================
+
+# more on reading images
+
+# As described earlier, the open() function of the Image module is used to open an image file. In most cases,
+# you simply pass it the filename as an argument.
+
+img = Image.open('sample.jpg')
+
+with open('sample.jpg', 'rb') as fp:
+    img = Image.open(fp)
+    # img.show()
+
+# you can use ContaineIO or TarIO module to access image file embedded in a larger file.
+# Reading from a tar archive.
+img = TarIO.TarIO('sample.tar', 'sample.jpg')
+img = Image.open(img)
+# img.show()
