aa44e1be1bf4e59ef398d247929ef5017676ae26
Mrinal Sinha <themrinalsinha@gmail.com>
2019-10-22 01:27:07 +0530

    the python steering council

---
+++ python38/steering_council.py
@@ -0,0 +1,14 @@
+# =====================================
+# ==== THE PYTHON STEERING COUNCIL ====
+# =====================================
+
+# Technically, Python’s governance is not a language feature. However, Python 3.8 is
+# the first version of Python not developed under the benevolent dictatorship of
+# Guido van Rossum. The Python language is now governed by a steering council
+# consisting of five core developers:
+#       -> Barry Warsaw
+#       -> Brett Cannon
+#       -> Carol Willing
+#       -> Guido van Rossum
+#       -> Nick Coghlan
+
