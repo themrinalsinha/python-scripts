2219e530bc6836b29dd95fd558ac85cb0d523d71
Mrinal Sinha <themrinalsinha@gmail.com>
2019-10-26 01:05:00 +0530

    workaround for wraps

--- decorators/wraps_thing.py
+++ decorators/wraps_thing.py
@@ -29,6 +29,38 @@ print(add.__doc__)
 print(mysum(1, 2, 3, 4, 5))
 print(mysum.__doc__)
 
-# Fantastic!
+# Fantastic! We get each function's result back, as a string, with
+# the exclamation points the decorator worked, But there are few issues
+# which we noticed, eg. if we ask for function name or doc it returns
+# wrapper docstring or name, to fix this we use wraps from functools.
+
+# There is a way to solve this partially, by assigning the __name__ and __doc__
+# attributes in our decorator.
+# eg:
+def mydeco(func):
+    def wrapper(*args, **kwargs):
+        return f'{func(*args, **kwargs)}!!!!'
+    wrapper.__name__ = func.__name__
+    wrapper.__doc__  = func.__doc__
+    return wrapper
+
+@mydeco
+def add(a, b):
+    '''
+    This is an add function
+    '''
+    return a + b
+
+@mydeco
+def mysum(*args):
+    '''
+    This is my sum funciton
+    '''
+    return sum(args)
+
+print(add(1, 2))
+print(add.__doc__)
+print(mysum(1, 2, 3, 4, 5))
+print(mysum.__doc__)
 print('\n\n=====================================================\n\n')
 
