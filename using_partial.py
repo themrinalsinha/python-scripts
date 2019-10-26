bdf981d3403492824671debb330f0bdd96dd376a
Mrinal Sinha <themrinalsinha@gmail.com>
2019-10-26 12:31:40 +0530

    basic use of partial function

--- using_partial.py
+++ using_partial.py
@@ -5,3 +5,15 @@ def multiply(x, y):
 
 double = partial(multiply, 2)
 print(double(5))
+
+# Partial function allows us to fix a certain number of arguments of a
+# function and generates a new function.
+
+# A normal function
+def f(a, b, c, x):
+    return 1000*a + 100*b + 10*c + x
+
+# A partial function that calls f with
+# a = 3, b = 1, and c = 4
+g = partial(f, 3, 1, 4)
+print(g(3))
