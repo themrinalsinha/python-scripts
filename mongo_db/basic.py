cc9357931dd593d9ac3e759d33ac9f40545f6958
Mrinal Sinha <themrinalsinha@gmail.com>
2020-01-27 16:06:23 +0530

    updating sorting and searching

--- mongo_db/basic.py
+++ mongo_db/basic.py
@@ -13,7 +13,7 @@ data = [
 ]
 
 # inserting multiple records at once.
-mycollection.insert_many(data)
+# mycollection.insert_many(data)
 print(client.list_database_names())
 
 # for finding in collection
@@ -22,3 +22,29 @@ print(res)
 res = mycollection.find()      # it returns the entire record which is feeded into the database
 for rec in res:
     print(rec)
+
+# custom query
+query = {"name": "John"}           # normal query
+query = {"name": {"$regex": "^D"}} # query search using regex - name starting with D
+
+for rec in mycollection.find(query):
+    print(rec)
+
+
+# sorting data
+for rec in mycollection.find().sort('name', 1): # sorting in ascending order
+    print("ASCENDING: ", rec)
+
+for rec in mycollection.find().sort('name', -1): # sorting in descending order
+    print("DESCENDING: ", rec)
+
+# deleting operations
+query = {"name": "Mrinal Sinha"}
+mycollection.delete_many(query)
+for rec in mycollection.find():
+    print(rec)
+
+# update operations
+query = {"name": {"$regex": "^Dha"}}
+new_values = {"$set": {"name": "Dhwani", "email": "dhwani@________.com"}}
+mycollection.update_many(query, new_values)
