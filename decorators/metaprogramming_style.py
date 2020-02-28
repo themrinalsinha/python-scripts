e228f9536a7618d3da89e88224c4a53e87235612
Mrinal Sinha <themrinalsinha@gmail.com>
2020-02-28 16:46:52 +0530

    callable

--- decorators/metaprogramming_style.py
+++ decorators/metaprogramming_style.py
@@ -9,3 +9,15 @@ def func():
 print(f"Type of function: {type(func)}")
 print(f"Type of type of function: {type(type(func))}")
 print(f"Bases of type of function: {type(func).__bases__}")
+
+
+# callables
+"""
+while python has the well-defined function class seen in the above example, it
+relies more on the presence of the __call__ method. That is, in python any object
+can act as a function, provided that it has this method, which is invoked when the
+object is "called"
+"""
+def func_callable():
+    pass
+print(f"Function is callable: {func_callable.__call__}")
