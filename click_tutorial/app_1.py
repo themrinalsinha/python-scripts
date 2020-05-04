5d752a8b47509d29361f9e30870b5f7089656566
Mrinal Sinha <themrinalsinha@gmail.com>
2020-05-04 23:41:16 +0530

    starting with click options

--- click_tutorial/app_1.py
+++ click_tutorial/app_1.py
@@ -27,8 +27,23 @@ def main(name, salary):
     click.echo(f"Hello, {name} !! Salary: {sum(salary)}")
 # ==============================================
 # $ python app_1.py -n Mrinal -s 21 21
-# $ Hello, Mrinal !! Salary: ('21', '21')
+# $ Hello, Mrinal !! Salary: 42
 # ==============================================
+
+# working with multiple options
+@click.command()
+@click.option('--name', '-n', default="Sinha", help='Username to print if any')
+@click.option('--salary', '-s', nargs=2, type=int)
+@click.option('--location', '-l', multiple=True) # taking multiple inputs
+def main(name, salary, location):
+    # print(f"Hello, {name} !! Salary: {sum(salary)}")
+    click.echo(f"Hello, {name} !! Salary: {sum(salary)}")
+    click.echo(f"Locations: {' |'.join(location)}")
+# ======================================================
+# $ python app_1.py -n Mrinal -s 24 24 -l Mumbai -l paris -l india -l kenya
+# $ Hello, Mrinal !! Salary: 48
+# $ Locations: Mumbai |paris |india |kenya
+# ======================================================
 
 if __name__ == '__main__':
     main()
