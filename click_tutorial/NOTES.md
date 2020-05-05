916361b6c01d499fdda04980f1cfb78ab1dac723
Mrinal Sinha <themrinalsinha@gmail.com>
2020-05-05 10:37:27 +0530

    working with options (default, nargs, type)

--- click_tutorial/NOTES.md
+++ click_tutorial/NOTES.md
@@ -2,3 +2,45 @@
 
 Installation: `$ pip install click`
 
+#### Working with "option"
+`option` is an optional parameter for any command
+- @click.option(large_name, small_name, default_value, help_text)
+```python
+import click
+@click.command()
+@click.option('--name', '-n', default="Sinha", help='your name please')
+def main(name):
+    print(f"Hello, {name}")
+```
+```shell
+$ python app.py
+$ Hello, Sinha
+-----------------------------
+$ python app.py --name Mrinal
+$ Hello, Mrinal
+-----------------------------
+NOTE: if default value is not
+given then when you run:
+$ python app.py
+$ Hello, None
+```
+- if you want to pass certain no of arguments use "nargs"
+```python
+@click.option('--coordinates', '-c', nargs=2)
+# if the option --coordinates is passed it will be
+$ python app.py --coordinates 101 201
+# if it is less than or greater than 2 it will throw error
+```
+- passing multiple values for any particular option
+```python
+@click.option('--places', '-p', multiple=True)
+# then you can pass -p multiple times eg:
+$ python app.py -p India -p USA -p Peru
+```
+- specifying type of input explicitly
+```python
+@click.option('--salary', '-s', type=int)
+$ python app.py -s "102" -> 102
+$ python app.py -s 102 -> 102
+$ python app.py -s "abc" -> throw error
+```
--- click_tutorial/app_1.py
+++ click_tutorial/app_1.py
@@ -1,49 +1,22 @@
 import click
 
-# @click.command()
-# @click.option('--name', '-n', default="Sinha", help='Username to print if any') # optional fields
-# def main(name):
-#     print(f"Hello, {name}")
-# # =================================
-# # $ python app_1.py --name/-n Mrinal
-# # $ Hello, Mrinal
-# # $ python app_1.py
-# # $ Hello, Sinha
-# # If Sinha is not given as default,
-# # It will print "Hello, None"
-# # =================================
-
-# # working with multiple values
-# @click.command()
-# @click.option('--name', '-n', nargs=2) # if passed --name/-n then 2 values are required
-# def main(name):
-#     print(f"Hello, {name}")
-
 @click.command()
-@click.option('--name', '-n', default="Sinha", help='Username to print if any')
-@click.option('--salary', '-s', nargs=2, type=int) # specifying input type
-def main(name, salary):
-    # print(f"Hello, {name} !! Salary: {sum(salary)}")
-    click.echo(f"Hello, {name} !! Salary: {sum(salary)}")
-# ==============================================
-# $ python app_1.py -n Mrinal -s 21 21
-# $ Hello, Mrinal !! Salary: 42
-# ==============================================
-
-# working with multiple options
-@click.command()
-@click.option('--name', '-n', default="Sinha", help='Username to print if any')
-@click.option('--salary', '-s', nargs=2, type=int)
-@click.option('--location', '-l', multiple=True) # taking multiple inputs
-def main(name, salary, location):
-    # print(f"Hello, {name} !! Salary: {sum(salary)}")
-    click.echo(f"Hello, {name} !! Salary: {sum(salary)}")
-    click.echo(f"Locations: {' |'.join(location)}")
-# ======================================================
-# $ python app_1.py -n Mrinal -s 24 24 -l Mumbai -l paris -l india -l kenya
-# $ Hello, Mrinal !! Salary: 48
-# $ Locations: Mumbai |paris |india |kenya
-# ======================================================
+@click.option('--name', '-n', default="User", help="Enter your name")
+@click.option('--marks', '-m', nargs=3, help="PCM marks", type=int)
+@click.option('--total', '-t', type=int, help="total marks")
+def main(name, marks, total):
+    click.echo(f"Hello, {name}")
+    if marks and total:
+        click.echo(f"PCM total: {sum(marks)}")
+        click.echo(f"Total of: {total}")
+        click.echo(f"Percentage: {(sum(marks)/total) * 100}")
 
 if __name__ == '__main__':
     main()
+
+# eg:
+# $ python app_1.py --name Mrinal -m 48 29 45 --total 150
+# $ Hello, Mrinal
+# $ PCM total: 122
+# $ Total of: 150
+# $ Percentage: 81.33333333333333
