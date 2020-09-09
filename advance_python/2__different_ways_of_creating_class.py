2ef8b35d9a719d8b0d519a154a54310e6be3f4bb
Mrinal Sinha <themrinalsinha@gmail.com>
2020-09-09 07:24:38 +0530

    creating class using metaclass

--- advance_python/2__different_ways_of_creating_class.py
+++ advance_python/2__different_ways_of_creating_class.py
@@ -1,4 +1,6 @@
+# =================================================================
 # 3 ways of creating classes (without using metaclass)
+# =================================================================
 
 from typing import Any
 
@@ -30,3 +32,40 @@ class C(object):
 
 c = C()
 print(c)
+
+# ====================================================================
+# 3 ways of creating classes using metaclasses
+# ====================================================================
+
+# NOTE: by default all the objects in python inherits from "type", eg: print(type(int)) --> <type>
+
+# METHOD - 4
+
+# Defining a base class, inheriting from "type"
+class D(type):
+    def __call__(cls, *args, **kwargs):
+        instance = super(D, cls).__call__(*args, **kwargs)
+        return instance
+
+    def __init__(cls, name, base, attr):
+        super(D, cls).__init__(name, base, attr)
+
+class E(metaclass=D):
+    pass
+
+e = E()
+print(e)
+
+# Or rewriting it differently
+class Meta(type):
+    def __new__(cls, *args, **kwargs):
+        return super(Meta, cls).__new__(cls, *args, **kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super(Meta, self).__init__(*args, **kwargs)
+
+class MyClass(metaclass=Meta):
+    def __init__(self) -> None:
+        pass
+mc = MyClass()
+print(mc)
