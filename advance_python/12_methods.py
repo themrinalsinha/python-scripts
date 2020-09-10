7ceafcf2bc583f75cc2c8c06d4375a9f5c3410ed
Mrinal Sinha <themrinalsinha@gmail.com>
2020-09-10 21:47:29 +0530

    different ways of calling methods

---
+++ advance_python/12_methods.py
@@ -0,0 +1,14 @@
+
+f = lambda: print("Hello World !! :)")
+
+# different ways of calling functions
+f()
+f.__call__()
+exec("f()")
+
+
+A = lambda: print("Hello from A")
+B = lambda: print("Hello from B")
+C = lambda: print("Hello from C")
+D = lambda: print("Hello from D")
+
