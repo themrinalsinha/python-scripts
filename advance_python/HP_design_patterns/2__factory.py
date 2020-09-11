beeb5a38b0e1017c9c307099e1293843413652fd
Mrinal Sinha <themrinalsinha@gmail.com>
2020-09-11 11:07:29 +0530

    abstract factory pattern

--- advance_python/HP_design_patterns/2__factory.py
+++ advance_python/HP_design_patterns/2__factory.py
@@ -8,8 +8,6 @@ helps to hide logic of creating objects.
 
 from abc import ABCMeta, abstractmethod
 
-from multiprocessing.util import info
-
 class Person(metaclass=ABCMeta):
     @abstractmethod
     def create(self):
@@ -109,3 +107,74 @@ for deg in profile.getDegrees():
 
 # =======================================================
 print('=' * 75)
+
+# ABSTRACT FACTORY PATTERN
+"""
+Provide an interface for creating families of related or dependent
+objects without specifying their concrete classes
+"""
+class CarFactory(metaclass=ABCMeta):
+    @abstractmethod
+    def build_parts(self):
+        pass
+
+    @abstractmethod
+    def build_cars(self):
+        pass
+
+class SedanCarFactory(CarFactory):
+    def build_parts(self):
+        return SedanCarPartsFactory()
+
+    def build_cars(self):
+        return SedanCarAssembleFactory()
+
+class SUVCarFactory(CarFactory):
+    def build_parts(self):
+        return SUVCarPartsFactory()
+
+    def build_cars(self):
+        return SUVCarAssembleFactory()
+
+
+class CarPartsFactory(metaclass=ABCMeta):
+    """ Declare an interface for a type of car parts """
+    @abstractmethod
+    def build(self):
+        pass
+
+class SedanCarPartsFactory(CarPartsFactory):
+    def build(self):
+        print("Sedan car parts are built")
+
+    def __str__(self) -> str:
+        return "<Sedan car parts>"
+
+class SUVCarPartsFactory(CarPartsFactory):
+    def build(self):
+        print("SUV car parts are built")
+
+    def __str__(self) -> str:
+        return "<SUV car parts>"
+
+class CarAssembleFactory(metaclass=ABCMeta):
+    """ Declare an interface for assembly of car """
+    @abstractmethod
+    def assemble(self):
+        pass
+
+class SedanCarAssembleFactory(CarAssembleFactory):
+    def assemble(self, parts):
+        print(f"Sedan car is assemble here using {parts}")
+
+class SUVCarAssembleFactory(CarAssembleFactory):
+    def assemble(self, parts):
+        print(f"Sedan car is assemble here using {parts}")
+
+
+
+for factory in (SedanCarFactory(), SUVCarFactory()):
+    car_parts = factory.build_parts()
+    car_builder = factory.build_cars()
+    car_parts.build()
+    car_builder.assemble(car_parts)
