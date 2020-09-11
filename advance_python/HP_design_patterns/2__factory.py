d76e1a590e4d38b050ed7b03b1eed7d2175c9469
Mrinal Sinha <themrinalsinha@gmail.com>
2020-09-11 10:37:23 +0530

    factory method pattern

--- advance_python/HP_design_patterns/2__factory.py
+++ advance_python/HP_design_patterns/2__factory.py
@@ -8,6 +8,8 @@ helps to hide logic of creating objects.
 
 from abc import ABCMeta, abstractmethod
 
+from multiprocessing.util import info
+
 class Person(metaclass=ABCMeta):
     @abstractmethod
     def create(self):
@@ -27,6 +29,74 @@ class PersonFactory(object):
         eval(destination)().create(name)
 
 
-designation = input("Enter designatin : ")
-name        = input("Person name      : ")
+# designation = input("Enter designatin : ")
+# name        = input("Person name      : ")
+designation = 'HR'
+name        = 'Mrinal Sinha'
 PersonFactory.createPerson(designation, name)
+
+# =======================================================
+print('=' * 75)
+
+# FACTORY METHOD PATTERN
+# this design pattern allows to create object but differs the decision
+# to the subclass, to determine class for object creation.
+
+class AbstractDegree(metaclass=ABCMeta):
+    @abstractmethod
+    def info(self):
+        pass
+
+class BE(AbstractDegree):
+    def info(self):
+        print("Bachelor of engineering...")
+
+    def __str__(self) -> str:
+        return "( Bachelor of engineering )"
+
+class ME(AbstractDegree):
+    def info(self):
+        print("Masters of engineering...")
+
+    def __str__(self) -> str:
+        return "( Masters of engineering )"
+
+class MBA(AbstractDegree):
+    def info(self):
+        print("Masters of business administration...")
+
+    def __str__(self) -> str:
+        return "( Masters of business administration )"
+
+
+class ProfileAbstractFactory(object):
+    def __init__(self) -> None:
+        self._degrees = []
+        self.createProfile()
+
+    @abstractmethod
+    def createProfile(self):
+        pass
+
+    def addDegree(self, degree):
+        self._degrees.append(degree)
+
+    def getDegrees(self):
+        return self._degrees
+
+class ManagerFactory(ProfileAbstractFactory):
+    def createProfile(self):
+        self.addDegree(BE())
+        self.addDegree(MBA())
+
+class EngineerFactory(ProfileAbstractFactory):
+    def createProfile(self):
+        self.addDegree(BE())
+        self.addDegree(ME())
+
+profile_type = input("Which profile would you like to create ? Manager/Engineer: ")
+profile = eval(f"{profile_type}Factory")()
+print(f"Creating profile of {profile_type}")
+print("Profile has following degrees")
+for deg in profile.getDegrees():
+    print(deg, end='|')
