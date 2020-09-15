2ff3044ff08b688df5499ee116e5c23994207ba1
Mrinal Sinha <themrinalsinha@gmail.com>
2020-09-15 09:54:13 +0530

    VERY VERY IMPORTANT: enabling custom typechecking on class and methods

--- advance_python/reinvention/5__typecheck_in_class.py
+++ advance_python/reinvention/5__typecheck_in_class.py
@@ -4,9 +4,8 @@ to write setter functions in our class, so how can we do that.
 without writing setter function and defining properties in our class
 """
 
-
-from ast import Str
-
+from inspect   import signature
+from functools import wraps
 
 class Contract(object):
     def __set__(self, instance, value):
@@ -55,6 +54,9 @@ class NonEmpty(Contract):
 class NonEmptyString(String, NonEmpty):
     pass
 
+class PositiveInteger(Integer, Positive):
+    pass
+
 class Player:
     name = NonEmptyString()
     x = Integer()
@@ -70,3 +72,59 @@ class Player:
 
     def right(self, dx):
         self.x += dx
+
+def checked(func):
+    _signature  = signature(func)
+    _annotation = func.__annotations__
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        bound = _signature.bind(*args, **kwargs)
+        for name, value in bound.arguments.items():
+            if name in _annotation:
+                _annotation[name].check(value)
+        return func(*args, **kwargs)
+    return wrapper
+
+class Base:
+    # 3.6+ feature
+    @classmethod
+    def __init_subclass__(cls) -> None:
+        # Applying check decorator
+        for name, val in cls.__dict__.items():
+            if callable(val):
+                setattr(cls, name, checked(val))
+
+        # Instantiate the contracts
+        for name, value in cls.__annotations__.items():
+            contract = value()
+            contract.__set_name__(cls, name)
+            setattr(cls, name, contract)
+
+    def __init__(self, *args):
+        ann = self.__annotations__
+        assert len(args) == len(ann), f"Expected {len(ann)} arguments"
+
+        # 3.6+ Ordered dictionary feature
+        for name, value in zip(ann, args):
+            setattr(self, name, value)
+
+    def __repr__(self) -> str:
+        args = ','.join(repr(getattr(self, name)) for name in self.__annotations__)
+        return f"{type(self).__name__}({args})"
+
+
+# complete type checking python
+class Player(Base):
+    # python 3.6+ class annotation feature
+    name: NonEmptyString
+    x   : Integer
+    y   : Integer
+
+    def left(self, dx: PositiveInteger):
+        self.x -= dx
+
+    def right(self, dx: PositiveInteger):
+        self.x += dx
+
+print(Player.__annotations__)
