401e180bdf8c710761484a66429bf84bafafbff7
Mrinal Sinha <themrinalsinha@gmail.com>
2020-10-28 09:45:47 +0530

    using different search queries match/match_phrase/term

--- elastic_search/app.py
+++ elastic_search/app.py
@@ -18,10 +18,9 @@ print(f"\nCheck if 'first_index' exists: {first_index_status}")
 delete_ack = es.indices.delete(index="first_index")
 print(f"\nDelete index (acknowledgment): {delete_ack}")
 
+
 # ============ Insert / Get Query ============
 # sample document
-es.indices.delete('cities')
-
 doc_1 = {"city": "New Delhi", "country": "India"}
 doc_2 = {"city": "London", "country": "England"}
 doc_3 = {"city": "Los Angeles", "country": "USA"}
@@ -44,3 +43,39 @@ print(f"\nInserted doc - 4: {_inserted}")
 result = es.get(index="cities", id=2)
 print(f"\nResult for ID-2 is: {result}")
 print(f"DATA for ID-2: {result['_source']}")
+es.indices.delete('cities')
+
+
+# ============ different search query for matching document ============
+# sample document
+doc_1 = {"sentence": "Today is a sunny day"}
+doc_2 = {"sentence": "Today is a bright-sunny day"}
+doc_3 = {"sentence": "Tomorrow is day after today"}
+doc_4 = {"sentence": "Bored AF"}
+
+# inserting records
+es.index(index="english", id=1, body=doc_1)
+es.index(index="english", id=2, body=doc_2)
+es.index(index="english", id=3, body=doc_3)
+es.index(index="english", id=4, body=doc_4)
+
+# search query
+result = es.search(index="english", body={"from": 0, "size": 0, "query": {"match": {"sentence": "SUNNY"}}})
+print(f"\nRESULT (1): {result}")
+
+result = es.search(index="english", body={"from": 0, "size": 2, "query": {"match": {"sentence": "SUNNY"}}})
+print(f"\nRESULT (2): {result}")
+
+# match query
+result = es.search(index="english", body={"from": 0, "size": 0, "query": {"match_phrase": {"sentence": "bright sunny"}}})
+print(f"\nSearch using match_phrase: {result}")
+
+result = es.search(index="english", body={"from": 0, "size": 1, "query": {"match_phrase": {"sentence": "bright sunny"}}})
+print(f"\nSearch using match_phrase: {result}")
+
+# using term query when you want to match the exact value
+result = es.search(index="english", body={"from": 0, "size": 0, "query": {"term": {"sentence": "Bored AF"}}})
+print(f"\nResult (term search): {result}")
+
+result = es.search(index="english", body={"from": 0, "size": 1, "query": {"term": {"sentence": "Bored AF"}}})
+print(f"\nResult (term search): {result}")
